d0b8858b1d6a3072821e368c93ee17ad83248f7a
Christopher Hillman <katie5five.5@gmail.com>
2021-10-15 16:33:38 +0200

    Set Up Mongo Connection

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ let mongoose = require("mongoose");
 
 let app = express();
 
+//CONNECT TO MONGODB
+mongoose.connect("mongodb://localhost:27017/myCapstoneDB", { useUnifiedTopology: true, useNewUrlParser: true }, (err)=>{
+    if(!err){
+        console.log("connected to DB")
+    }else{
+        console.log("Error in db connection")
+    }
+})
 
 
 
 
+//LISTEN ON PORT 3001
 app.listen(3001, console.log("Server Listening on Port 3001"))
