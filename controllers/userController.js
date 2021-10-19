c2cd28bfddf1f685eb2e8a47e04e4e6c10611fc3
Christopher Hillman <katie5five.5@gmail.com>
2021-10-19 15:22:20 +0200

    getUser method added, with password encryption

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,32 +4,75 @@ const bcrypt = require("bcrypt");
 const User = require("../schemas/userSchema");
 
 
+//CREATE NEW USER (ADMIN OR USER)
 let createUser = async (req, res) => {
     
     let { uname, email, utype, password } = req.body;
 
-    const saltRounds = 12;
- 
+    let userExists;
     let newUser;
-        try{
-        newUser = await new User({
-            uid: uid(),
-            uname,
-            email,
-            utype,
-            password: await bcrypt.hash(password, saltRounds)
-        })
 
-        await newUser.save((err, data)=>{
-            if(!err){
-                res.status(201).json(data)
-            }else{
-                res.status(500).send("An error has occured whilst saving user, please try again!")
-            }
-        }); 
+    const saltRounds = 12;
+
+        try{
+            userExists = await User.findOne({email: email})
         }catch(err){
-            res.status(500).send("Unexpected Error")
+            res.send("An unexpected error occured!!")
+        }
+
+        if(!userExists){
+            try{
+            newUser = await new User({
+                uid: uid(),
+                uname,
+                email,
+                utype,
+                password: await bcrypt.hash(password, saltRounds)
+            })
+
+            await newUser.save((err, data)=>{
+                if(!err){
+                    res.status(201).json(data)
+                }else{
+                    res.status(500).send("An error has occured whilst saving user, please try again!")
+                }
+            }); 
+            }catch(err){
+                res.status(500).send("Unexpected Error")
+            }
+        }else{
+            res.send("A user with this email already exists")
         }
     }
 
-module.exports = { createUser }
+    //RETREIVE A USER ACCOUNT
+    let getUser = async (req, res) => {
+
+        let { email, password } = req.body;
+        let user;
+        let isPassword
+        try{
+            user = await User.findOne({email: email});
+
+            if(user){
+
+              isPassword = await bcrypt.compare(password, user.password)
+                
+                if(isPassword == true){
+                    res.json(user);
+                }else{
+                    res.send("password entered is incorrect");
+                }
+                
+            }else{
+                res.send("unable to find user");
+            }
+        }catch(err){
+            res.send("Unknown Fail");
+        }
+    }
+
+    //EDIT A USER ACCOUNT
+    
+
+module.exports = { createUser, getUser }
